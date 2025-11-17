a8577ae5bc218ee7f302bd61896fa533235a7713
Yuzar <yuzar@Yuzars-MacBook-Air.local>
2025-11-17 11:38:34 +0700

    fix(vite.config): fix build config

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,56 @@
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import { glob } from "glob";
 import { resolve } from "path";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the directory name of the current module
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Find all .js files in the src directory
+const jsFiles = glob.sync("src/**/*.js").reduce((acc, file) => {
+  const name = path.basename(file, ".js");
+  acc[name] = fileURLToPath(new URL(file, import.meta.url));
+  return acc;
+}, {});
 
 export default defineConfig({
-	base: "./",
-	plugins: [glsl()],
-	build: {
-		outDir: "dist",
-		minify: "terser",
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "src/main.js"),
-			},
-			output: {
-				entryFileNames: "[name].js",
-				chunkFileNames: "[name].js",
-				assetFileNames: "[name].[ext]",
-				format: "iife",
-			},
-		},
-		reportCompressedSize: true,
-	},
-	server: {
-		port: 5173,
-		host: true,
-		cors: true,
-		hmr: {
-			protocol: "ws",
-			host: "localhost",
-			port: 5173,
-		},
-		watch: {
-			usePolling: true,
-		},
-	},
-	preview: {
-		port: 4173,
-		cors: true,
-	},
+  base: "./",
+  plugins: [glsl()],
+  build: {
+    outDir: "dist",
+    minify: "terser",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/main.js"),
+        ...jsFiles,
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+        // format: "iife",
+        inlineDynamicImports: false,
+      },
+    },
+    reportCompressedSize: true,
+  },
+  server: {
+    port: 5173,
+    host: true,
+    cors: true,
+    hmr: {
+      protocol: "ws",
+      host: "localhost",
+      port: 5173,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  preview: {
+    port: 4173,
+    cors: true,
+  },
 });
